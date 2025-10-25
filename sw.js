ac8f2d0438adf22611ebe690995864caf64e80a5
kev1470 <kevin_holstein@gmx.de>
2025-10-25 18:12:17 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'welsing-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // Network-first for same-origin HTML, cache-first for others
+  if (url.origin === location.origin && url.pathname.endsWith('.html')) {
+    e.respondWith(fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html'))));
+  }
+});
